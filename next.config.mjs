9f1ab253fc85632b01c5533fa82859b99b289c0f
ArQdev <shemsine@gmail.com>
2026-05-17 13:17:10 +0200

    Add ESLint and TypeScript ignore options
    Added ESLint and TypeScript configuration options to ignore errors during builds.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/arqdev-site',
-    assetPrefix: '/arqdev-site/',
-    images: { unoptimized: true },
-    trailingSlash: true,
+  output: 'export',
+  basePath: '/arqdev-site',
+  assetPrefix: '/arqdev-site/',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
